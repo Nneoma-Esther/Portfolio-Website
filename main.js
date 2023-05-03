9f041c28cf51588cf520f07d53ff7c31650ad798
Nneoma <nneomaokechi@gmail.com>
2023-05-03 16:42:47 +0100

    Initial Commit

---
+++ main.js
@@ -0,0 +1,15 @@
+
+AOS.init();
+var typed = new Typed('#typed', {
+    strings: ['Frontend Developer', 'Product Designer'],
+    backSpeed: 40,
+    typeSpeed: 40,
+    loop: true,
+    // 
+    // loopCount: infinity
+  });
+// var typed= new Typed('#typed',{
+//   stringElement: '#typed-strings',
+//   backSpeed: 40,
+//   typeSpeed: 40
+// });
